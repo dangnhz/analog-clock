088394429c25f23e9fd1be639bc5c62bc51d1f9d
Dang Nhz <41246555+dangnhz@users.noreply.github.com>
2020-06-02 00:16:39 +1000

    add dark mode

--- main.js
+++ main.js
@@ -14,9 +14,29 @@ function setClock() {
   let minuteRatio = minute * 6 + secondRatio / 60;
   let hourRatio = hour * 30 + minuteRatio / 12;
 
-  secondHand.style.transform = `translateX(-50%) rotate(${secondRatio}deg)`;
-  minuteHand.style.transform = `translateX(-50%)  rotate(${minuteRatio}deg)`;
-  hourHand.style.transform = ` translateX(-50%)  rotate(${hourRatio}deg)`;
+  secondHand.style.transform = ` translate(-50%, 20%) rotate(${secondRatio}deg)`;
+  minuteHand.style.transform = ` translateX(-50%) rotate(${minuteRatio}deg)`;
+  hourHand.style.transform = `  translateX(-50%) rotate(${hourRatio}deg)`;
 }
 
 setClock();
+
+// change dark mode
+const container = document.getElementById('container');
+const switcher = document.getElementById('switch');
+
+let isDarkMode = false;
+
+switcher.addEventListener('click', () => {
+  if (!isDarkMode) {
+    switcher.classList.add('active');
+    container.classList.remove('light');
+    container.classList.add('dark');
+    isDarkMode = true;
+  } else {
+    switcher.classList.remove('active');
+    container.classList.remove('dark');
+    container.classList.add('light');
+    isDarkMode = false;
+  }
+});
